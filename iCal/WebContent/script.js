cdc53e4aad8a2a82086fda6c5f9bf3c3af3605bf
Łukasz Wiśniewski <lucas.katanax@gmail.com>
2016-05-28 20:39:29 +0200

    [Łukasz Wiśniewski] Animacja przycisków, dodano ikony

---
+++ iCal/WebContent/script.js
@@ -0,0 +1,19 @@
+/**
+ * Scrolling Script
+ */
+$(document).ready(function(){
+	
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
